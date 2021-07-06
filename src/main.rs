397a69d76d1f8d38f5215ec2307eef06e92e1186
Lorenzo Limoli <lorenzo.limoli@outlook.it>
2021-07-06 17:00:52 +0200

    shared root channel

--- src/main.rs
+++ src/main.rs
@@ -5,7 +5,7 @@ use anyhow::Result;
 use std::sync::Mutex;
 
 pub struct AppState{
-    root: Mutex<Box<RootChannel>>
+    root: Mutex<RootChannel>
 }
 
 #[derive(Serialize)]
@@ -16,11 +16,16 @@ pub struct Message{
 #[get("/{name}")]
 async fn hello(web::Path(name): web::Path<String>, data: web::Data<AppState>) -> impl Responder {
     if name.starts_with("l"){
-        // let root = data.root.borrow();
-        // let info = format!("{:#?}", root.channel_info());
-        return HttpResponse::Unauthorized().body("");
+        let root = data.root.lock().unwrap();
+        let info = format!("{:#?}", root.channel_info());
+        return HttpResponse::Unauthorized().body(info);
     }
     HttpResponse::Ok().json(Message{msg: format!("Hello {}!", name)})
+}
+
+#[get("/")]
+async fn welcome() -> impl Responder{
+    HttpResponse::Ok().body("Welcome to BioEnPro4To gateway")
 }
 
 #[actix_web::main]
@@ -28,11 +33,12 @@ async fn main() -> Result<()> {
     let mut root = RootChannel::new(false);
     let info = root.open("psw").await?;
     println!("Root Channel -> {}:{}", info.channel_id(), info.announce_id());
-
-    let state = web::Data::new(AppState{root: Mutex::new(Box::new(root))});
+    println!("Open at http://localhost:8080");
+    let state = web::Data::new(AppState{root: Mutex::new(root)});
     HttpServer::new(move || {
         App::new()
             .app_data(state.clone())
+            .service(welcome)
             .service(hello)
     })
         .bind("127.0.0.1:8080")?
